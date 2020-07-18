fb66202bba633f3a496ee721dc97eead17681de0
islai <islaifer2379@gmail.com>
2020-07-17 21:01:58 -0300

    link with main dir in C:

--- src/controller/DatabaseController.java
+++ src/controller/DatabaseController.java
@@ -1,5 +1,18 @@
 package controller;
 
-public class DatabaseController {
+import java.io.File;
 
+public class DatabaseController {
+	
+	private void verifMainDir() {
+		String path = "C:\\CRUD";
+		File dir = new File(path);
+		if(!dir.exists() && !dir.isDirectory()) {
+			if(dir.mkdir()) {
+				System.out.println("Dir create success!");
+			}else {
+				System.err.println("Error in dir create!");
+			}
+		}
+	}
 }
